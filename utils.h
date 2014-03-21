2dd909872281d73ef1200d887eaa8b829f60e8ca
berto <berto@lobosunidos.org>
2014-03-21 15:56:49 -0400

    std-11

--- utils.h
+++ utils.h
@@ -9,15 +9,26 @@
 
 #include <errno.h>
 #include <time.h>
+#include <unistd.h>
 /////////////////////////////////////////////////
+
 void debug(const std::string&);
+
 std::vector<std::string> split(char, const std::string&);
+
 void deWhiteSpace(std::string &);
+
 std::queue<std::string> vectorToQueue(const std::vector<std::string> &);
+
 std::deque<std::string> vectorToDeque(const std::vector<std::string> &);
+
 std::string replace(char, char, const std::string&);
+
 std::string join(char p, const std::vector<std::string> &list, int start=0, int end=-1);
+
 /////////////////////////////////////////////////
+
 int createDirecttory(const std::string &str);
+
 /////////////////////////////////////////////////
 #endif
